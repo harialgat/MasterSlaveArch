ec6c8c1fe784cd9cfd9e99ccdc8b9665cde4a38d
harish <harishalgat3802@gmail.com>
2023-04-05 14:37:35 +0530

    Initial commit

---
+++ src/main/java/com/vodafone/tv/concurrnecy/concurrencyrequest/Logout.java
@@ -0,0 +1,2 @@
+package com.vodafone.tv.concurrnecy.concurrencyrequest;public class Logout {
+}
---
+++ src/main/java/com/vodafone/tv/concurrnecy/yamlparser/pojos/Domains.java
@@ -0,0 +1,13 @@
+package com.vodafone.tv.concurrnecy.yamlparser.pojos;
+
+public class Domains {
+    private Domain domain = new Domain();
+
+    public Domain getDomain() {
+        return domain;
+    }
+
+    public void setDomain(Domain domain) {
+        this.domain = domain;
+    }
+}
